95243a47aa97dd272205997e2c6f1fc2f5a18d3e
Michael Rans <rans@email.com>
2017-10-11 17:43:55 +0200

    Use World Bank data for locations

--- src/hdx/utilities/location.py
+++ src/hdx/utilities/location.py
@@ -1,39 +1,156 @@
 # -*- coding: utf-8 -*-
 """Location utilities"""
-from typing import List, Callable, Tuple, Optional, TypeVar
+import logging
+from typing import List, Callable, Tuple, Optional, TypeVar, Any
 
-import geonamescache
+from hdx.utilities.downloader import Download, DownloadError
+from hdx.utilities.loader import load_json
+from hdx.utilities.path import script_dir_plus_file
 
 ExceptionUpperBound = TypeVar('T', bound='Exception')
 
 
+logger = logging.getLogger(__name__)
+
+
 class Location(object):
-    """Methods to help with countries and continents
+    """Location class with various methods to help with countries and regions. Uses World Bank API unless countries
+    argument supplied with list of dicts of form:
+    {'name': 'Afghanistan', 'region': {'value': 'South Asia', 'id': 'SAS'}, 'capitalCity': 'Kabul', 'id': 'AFG',
+    'lendingType': {'value': 'IDA', 'id': 'IDX'}, 'adminregion': {'value': 'South Asia', 'id': 'SAS'}, 'iso2Code': 'AF',
+    'longitude': '69.1761', 'incomeLevel': {'value': 'Low income', 'id': 'LIC'}, 'latitude': '34.5228'}
+
+    Args:
+        countries (Optional[List]): List of countries in same format as World Bank API. Defaults to None.
+        url (Optional[str]): URL of World Bank API. Defaults to 'http://api.worldbank.org/countries?format=json&per_page=10000'.
     """
 
-    gc = geonamescache.GeonamesCache()
-    continents = gc.get_continents()
-    countries = gc.get_countries().values()
+    def __init__(self, countries=None, url='http://api.worldbank.org/countries?format=json&per_page=10000'):
+        # type: (Optional[List]) -> None
+        if countries is None:
+            json = None
+            if url is not None:
+                try:
+                    response = Download().download(url)
+                    json = response.json()
+                except DownloadError:
+                    logger.exception('Download from API failed! Falling back to stored countries file.')
+            if json is None:
+                json = load_json(script_dir_plus_file('countries.json', Location))
+            countries = json[1]
+        self.countries = dict()
+        self.iso2iso3 = dict()
+        self.countrynames2iso3 = dict()
+        self.regions2countries = dict()
+        self.regionids2names = dict()
+        self.regionnames2ids = dict()
+        for country in countries:
+            if country['region']['value'] != 'Aggregates':
+                iso2 = country['iso2Code'].upper()
+                iso3 = country['id'].upper()
+                countryname = country['name']
+                regionid = country['region']['id']
+                regionname = country['region']['value']
+                self.countries[iso3] = country
+                self.iso2iso3[iso2] = iso3
+                self.countrynames2iso3[countryname.upper()] = iso3
+                region = self.regions2countries.get(regionid)
+                if region is None:
+                    region = set()
+                    self.regions2countries[regionid] = region
+                region.add(iso3)
+                self.regionids2names[regionid] = regionname
+                self.regionnames2ids[regionname.upper()] = regionid
+        for regionid in self.regions2countries:
+            self.regions2countries[regionid] = sorted(list(self.regions2countries[regionid]))
 
-    @staticmethod
-    def get_country_name_from_iso3(iso3):
-        # type: (str) -> Optional[str]
+    def get_country_info_from_iso3(self, iso3, exception=None):
+        # type: (str, Optional[ExceptionUpperBound]) -> Optional[Dict[str]]
+        """Get country information from iso3 code
+
+        Args:
+            iso3 (str): Iso 3 code for which to get country name
+            exception (Optional[ExceptionUpperBound]): An exception to raise if country not found. Defaults to None.
+
+        Returns:
+            Optional[Dict[str]]: country information
+        """
+        country = self.countries.get(iso3.upper())
+        if country is not None:
+            return country
+
+        if exception is not None:
+            raise exception
+        return None
+
+    def get_country_name_from_iso3(self, iso3, exception=None):
+        # type: (str, Optional[ExceptionUpperBound]) -> Optional[str]
         """Get country name from iso3 code
 
         Args:
             iso3 (str): Iso 3 code for which to get country name
+            exception (Optional[ExceptionUpperBound]): An exception to raise if country not found. Defaults to None.
 
         Returns:
             Optional[str]: country name
         """
-        iso3lower = iso3.lower()
-        for countrydetails in Location.countries:
-            if iso3lower == countrydetails.get('iso3').lower():
-                return countrydetails.get('name')
+        countryinfo = self.get_country_info_from_iso3(iso3, exception)
+        if countryinfo is not None:
+            return countryinfo['name']
         return None
 
-    @staticmethod
-    def get_iso3_country_code(country, exception=None):
+    def get_iso3_from_iso2(self, iso2, exception=None):
+        # type: (str, Optional[ExceptionUpperBound]) -> Optional[str]
+        """Get iso3 from iso2 code
+
+        Args:
+            iso2 (str): Iso 2 code for which to get country name
+            exception (Optional[ExceptionUpperBound]): An exception to raise if country not found. Defaults to None.
+
+        Returns:
+            Optional[str]: Iso 3 code
+        """
+        iso3 = self.iso2iso3.get(iso2.upper())
+        if iso3 is not None:
+            return iso3
+
+        if exception is not None:
+            raise exception
+        return None
+
+    def get_country_info_from_iso2(self, iso2, exception=None):
+        # type: (str, Optional[ExceptionUpperBound]) -> Optional[Dict[str]]
+        """Get country name from iso2 code
+
+        Args:
+            iso2 (str): Iso 2 code for which to get country name
+            exception (Optional[ExceptionUpperBound]): An exception to raise if country not found. Defaults to None.
+
+        Returns:
+            Optional[Dict[str]]: country information
+        """
+        iso3 = self.get_iso3_from_iso2(iso2, exception)
+        if iso3 is not None:
+            return self.get_country_info_from_iso3(iso3, exception)
+        return None
+
+    def get_country_name_from_iso2(self, iso2, exception=None):
+        # type: (str, Optional[ExceptionUpperBound]) -> Optional[str]
+        """Get country name from iso2 code
+
+        Args:
+            iso2 (str): Iso 2 code for which to get country name
+            exception (Optional[ExceptionUpperBound]): An exception to raise if country not found. Defaults to None.
+
+        Returns:
+            Optional[str]: country name
+        """
+        iso3 = self.get_iso3_from_iso2(iso2, exception)
+        if iso3 is not None:
+            return self.get_country_name_from_iso3(iso3, exception)
+        return None
+
+    def get_iso3_country_code(self, country, exception=None):
         # type: (str, Optional[ExceptionUpperBound]) -> Optional[str]]
         """Get iso 3 code for country. Only exact matches or None are returned.
 
@@ -44,22 +161,25 @@ class Location(object):
         Returns:
             Optional[str]: Return iso 3 country code or None
         """
-        countrylower = country.lower()
-        if len(countrylower) == 3:
-            for countrydetails in Location.countries:
-                if countrylower == countrydetails.get('iso3').lower():
-                    return countrylower
+        countryupper = country.upper()
+        len_countryupper = len(countryupper)
+        if len_countryupper == 3:
+            if countryupper in self.countries:
+                return countryupper
+        elif len_countryupper == 2:
+            iso3 = self.iso2iso3.get(countryupper)
+            if iso3 is not None:
+                return iso3
 
-        for countrydetails in Location.countries:
-            if countrylower == countrydetails.get('name').lower():
-                return countrydetails.get('iso3').lower()
+        iso3 = self.countrynames2iso3.get(countryupper)
+        if iso3 is not None:
+            return iso3
 
         if exception is not None:
             raise exception
         return None
 
-    @staticmethod
-    def get_iso3_country_code_partial(country, exception=None):
+    def get_iso3_country_code_partial(self, country, exception=None):
         # type: (str, Optional[ExceptionUpperBound]) -> Tuple[Optional[str], bool]]
         """Get iso 3 code for country. A tuple is returned with the first value being the iso 3 code and the second
         showing if the match is exact or not.
@@ -71,47 +191,34 @@ class Location(object):
         Returns:
             Tuple[Optional[str], bool]]: Return iso 3 code and if the match is exact or (None, False).
         """
-        iso3 = Location.get_iso3_country_code(country)
+        iso3 = self.get_iso3_country_code(country)
 
         if iso3 is not None:
             return iso3, True
 
-        countrylower = country.lower()
-        for countrydetails in Location.countries:
-            countryname = countrydetails.get('name').lower()
-            if countrylower in countryname or countryname in countrylower:
-                return countrydetails.get('iso3').lower(), False
+        countryupper = country.upper()
+        for countryname in self.countrynames2iso3:
+            if countryupper in countryname or countryname in countryupper:
+                return self.countrynames2iso3[countryname], False
 
         if exception is not None:
             raise exception
         return None, False
 
-    @staticmethod
-    def get_countries_in_continent(continent, function=lambda x: x):
-        # type: (str, Callable[[str], Any]) -> List[str]
+    def get_countries_in_region(self, region):
+        # type: (str, Any]) -> List[str]
         """Get countries (iso 3 codes) in continent
 
         Args:
-            continent (str): Two letter continent code or continent name
-            function (Callable[[str], Any]: Format of each list element. Defaults to str (iso 3 country code).
+            region (str): Three letter region code or region name
 
         Returns:
-            List(str): List of iso 3 country names
+            List(str): Sorted list of iso 3 country names
         """
-        continentcode = None
-        for code in Location.continents:
-            if continent.upper() == code:
-                continentcode = code
-                break
-            continentdetails = Location.continents[code]
-            if continent.lower() == continentdetails['name'].lower():
-                continentcode = code
-                break
-        countries = list()  # type: List[str]
-        if continentcode is None:
-            return countries
-        for country in Location.countries:
-            if country.get('continentcode') == continentcode:
-                countries.append(function(country.get('iso3').lower()))
-        return sorted(countries)
-
+        regionupper = region.upper()
+        if regionupper in self.regionids2names:
+            return self.regions2countries[regionupper]
+        regionid = self.regionnames2ids.get(regionupper)
+        if regionid is not None:
+            return self.regions2countries[regionid]
+        return list()
--- tests/hdx/utilities/test_location.py
+++ tests/hdx/utilities/test_location.py
@@ -1,42 +1,50 @@
 # -*- coding: UTF-8 -*-
-"""Location Tests"""
+"""location Tests"""
 import pytest
 
 from hdx.utilities.location import Location
 
 
 class TestLocation:
-    def test_get_country_name_from_iso3(self):
-        assert Location.get_country_name_from_iso3('jpn') == 'Japan'
-        assert Location.get_country_name_from_iso3('awe') is None
-        assert Location.get_country_name_from_iso3('Pol') == 'Poland'
-        assert Location.get_country_name_from_iso3('SGP') == 'Singapore'
-        assert Location.get_country_name_from_iso3('uy') is None
+    @pytest.fixture(scope='class')
+    def location(self):
+        return Location(url='NOTEXIST')
 
-    def test_get_iso3_country_code(self):
-        assert Location.get_iso3_country_code('jpn') == 'jpn'
-        assert Location.get_iso3_country_code_partial('jpn') == ('jpn', True)
-        assert Location.get_iso3_country_code_partial('ZWE') == ('zwe', True)
-        assert Location.get_iso3_country_code_partial('Vut') == ('vut', True)
-        assert Location.get_iso3_country_code('abc') is None
-        assert Location.get_iso3_country_code_partial('abc') == (None, False)
-        assert Location.get_iso3_country_code_partial('United Kingdom') == ('gbr', True)
-        assert Location.get_iso3_country_code_partial('united states') == ('usa', True)
-        assert Location.get_iso3_country_code('UZBEKISTAN') == 'uzb'
-        assert Location.get_iso3_country_code_partial('UZBEKISTAN') == ('uzb', True)
-        assert Location.get_iso3_country_code('Sierra') is None
-        assert Location.get_iso3_country_code_partial('Sierra') == ('sle', False)
-        assert Location.get_iso3_country_code('Venezuela (Bolivarian Republic of)') is None
-        assert Location.get_iso3_country_code_partial('Venezuela (Bolivarian Republic of)') == ('ven', False)
-        with pytest.raises(ValueError):
-            Location.get_iso3_country_code('abc', exception=ValueError)
-        with pytest.raises(ValueError):
-            Location.get_iso3_country_code_partial('abc', exception=ValueError)
+    def test_get_country_name_from_iso3(self, location):
+        assert location.get_country_name_from_iso3('jpn') == 'Japan'
+        assert location.get_country_name_from_iso3('awe') is None
+        assert location.get_country_name_from_iso3('Pol') == 'Poland'
+        assert location.get_country_name_from_iso3('SGP') == 'Singapore'
+        assert location.get_country_name_from_iso3('uy') is None
+        assert location.get_country_name_from_iso3('VeN') == 'Venezuela, RB'
 
-    def test_get_countries_in_continent(self):
-        assert len(Location.get_countries_in_continent('AF')) == 58
-        assert len(Location.get_countries_in_continent('eu')) == 54
-        assert len(Location.get_countries_in_continent('As')) == 52
-        assert len(Location.get_countries_in_continent('ab')) == 0
-        assert len(Location.get_countries_in_continent('North America')) == 42
-        assert len(Location.get_countries_in_continent('North America')) == 42
+    def test_get_country_name_from_iso2(self, location):
+        assert location.get_country_name_from_iso2('jp') == 'Japan'
+        assert location.get_country_name_from_iso2('ab') is None
+        assert location.get_country_name_from_iso2('Pl') == 'Poland'
+        assert location.get_country_name_from_iso2('SG') == 'Singapore'
+        assert location.get_country_name_from_iso2('SGP') is None
+        assert location.get_country_name_from_iso2('VE') == 'Venezuela, RB'
+
+    def test_get_iso3_country_code(self, location):
+        assert location.get_iso3_country_code('jpn') == 'JPN'
+        assert location.get_iso3_country_code_partial('jpn') == ('JPN', True)
+        assert location.get_iso3_country_code_partial('ZWE') == ('ZWE', True)
+        assert location.get_iso3_country_code_partial('Vut') == ('VUT', True)
+        assert location.get_iso3_country_code('abc') is None
+        assert location.get_iso3_country_code_partial('abc') == (None, False)
+        assert location.get_iso3_country_code_partial('United Kingdom') == ('GBR', True)
+        assert location.get_iso3_country_code_partial('united states') == ('USA', True)
+        assert location.get_iso3_country_code('UZBEKISTAN') == 'UZB'
+        assert location.get_iso3_country_code_partial('UZBEKISTAN') == ('UZB', True)
+        assert location.get_iso3_country_code('Sierra') is None
+        assert location.get_iso3_country_code_partial('Sierra') == ('SLE', False)
+        assert location.get_iso3_country_code('Venezuela') is None
+        assert location.get_iso3_country_code_partial('Venezuela') == ('VEN', False)
+        with pytest.raises(ValueError):
+            location.get_iso3_country_code('abc', exception=ValueError)
+        with pytest.raises(ValueError):
+            location.get_iso3_country_code_partial('abc', exception=ValueError)
+
+    def test_get_countries_in_region(self, location):
+        assert len(location.get_countries_in_region('SSF')) == 48
