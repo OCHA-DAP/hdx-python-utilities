f4b26af7870cd2c1f63ef1d5acaf3b354a13e928
Mike <rans@email.com>
2020-04-21 09:18:35 +0200

    New multiple_progress_storing_tempdir

--- src/hdx/utilities/path.py
+++ src/hdx/utilities/path.py
@@ -4,7 +4,7 @@ import contextlib
 import inspect
 import logging
 import sys
-from os import getenv, makedirs
+from os import getenv, makedirs, remove
 from os.path import abspath, realpath, dirname, join, exists
 from shutil import rmtree
 from tempfile import gettempdir
@@ -57,39 +57,49 @@ def script_dir_plus_file(filename, pyobject, follow_symlinks=True):
     return join(script_dir(pyobject, follow_symlinks), filename)
 
 
-def get_temp_dir(folder=None):
-    # type: (Optional[str]) -> str
+def get_temp_dir(folder=None, delete_if_exists=False, tempdir=None):
+    # type: (Optional[str], bool, Optional[str]) -> str
     """Get a temporary directory. Looks for environment variable TEMP_DIR and falls
-    back on os.gettempdir.
+    back on os.gettempdir if a root temporary directory is not supplied. If a folder is supplied, creates that folder
+     within the temporary directory. Optionally deletes and recreates it if it already exists.
 
     Args:
         folder (Optional[str]): Folder to create in temporary folder. Defaults to None.
+        delete_if_exists (bool): Whether to delete the folder if it exists. Defaults to False.
+        tempdir (Optional[str]): Folder to use as temporary directory. Defaults to None (TEMP_DIR or os.gettempdir).
 
     Returns:
         str: A temporary directory
     """
-    tempdir = getenv('TEMP_DIR', gettempdir())
+    if tempdir is None:
+        tempdir = getenv('TEMP_DIR', gettempdir())
     if folder:
         tempdir = join(tempdir, folder)
-        if not exists(tempdir):
+        if exists(tempdir):
+            if delete_if_exists:
+                rmtree(tempdir)
+                makedirs(tempdir)
+        else:
             makedirs(tempdir)
     return tempdir
 
 
 @contextlib.contextmanager
-def temp_dir(folder=None, delete_on_success=True, delete_on_failure=True):
-    # type: (Optional[str], bool, bool) -> str
+def temp_dir(folder=None, delete_if_exists=False, delete_on_success=True, delete_on_failure=True, tempdir=None):
+    # type: (Optional[str], bool, bool, bool, Optional[str]) -> str
     """Get a temporary directory optionally with folder appended (and created if it doesn't exist)
 
     Args:
         folder (Optional[str]): Folder to create in temporary folder. Defaults to None.
+        delete_if_exists (bool): Whether to delete the folder if it exists. Defaults to False.
         delete_on_success (bool): Whether to delete folder (if folder supplied) on exiting with statement successfully. Defaults to True.
         delete_on_failure (bool): Whether to delete folder (if folder supplied) on exiting with statement unsuccessfully. Defaults to True.
+        tempdir (Optional[str]): Folder to use as temporary directory. Defaults to None (TEMP_DIR or os.gettempdir).
 
     Returns:
         str: A temporary directory
     """
-    tempdir = get_temp_dir(folder)
+    tempdir = get_temp_dir(folder, delete_if_exists=delete_if_exists, tempdir=tempdir)
     try:
         yield tempdir
         if folder and delete_on_success:
@@ -101,64 +111,199 @@ def temp_dir(folder=None, delete_on_success=True, delete_on_failure=True):
             raise
 
 
-def progress_storing_tempdir(folder, iterator, key, store_batch=True, batch=None):
-    # type: (str, Iterable[Dict], str, bool, Optional[str]) -> Tuple[Dict,Dict]
-    """Yields 2 dictionaries. The first contains key tempdir which is the temporary directory optionally with folder
-    appended (and created if it doesn't exist). It will also contain the key batch containing a batch code to be
-    passed as the batch parameter in create_in_hdx or update_in_hdx calls. The second dictionary is the next dictionary
-    in the iterator. The folder persists until the final iteration and which iteration to start at and the batch code
-    is persisted between runs.
+def read_or_create_batch(folder, batch=None):
+    # type: (str, bool) -> str
+    """Get batch or create it if it doesn't exist
 
     Args:
-        folder (str): Folder to create in temporary folder
-        iterator (Iterable[Dict]): Iterate over this object persisting progress
-        key (str): Key to examine from dictionary from iterator
-        store_batch (bool): Whether to keep track of the batch in interrupted runs.
+        folder (str): Folder in which to look for or create batch file.
         batch (Optional[str]): Batch to use if there isn't one in a file already.
 
     Returns:
-        Tuple[Dict,Dict]: A tuple of the form (info directory, next object in iterator)
+        str: Batch
     """
-    with temp_dir(folder, delete_on_success=True, delete_on_failure=False) as tempdir:
-        info = {'tempdir': tempdir}
-        if store_batch:
-            batch_file = join(tempdir, 'batch.txt')
-            if exists(batch_file):
-                batch = load_file_to_str(batch_file, strip=True)
-                logger.info('File BATCH = %s' % batch)
-            else:
-                if not batch:
-                    batch = get_uuid()
-                    logger.info('Generated BATCH = %s' % batch)
-                save_str_to_file(batch, batch_file)
-            info['batch'] = batch
-        progress_file = join(tempdir, 'progress.txt')
-        wheretostart = getenv('WHERETOSTART')
-        if wheretostart:
-            wheretostart = wheretostart.upper()
-            if wheretostart == 'RESET':
-                rmtree(tempdir)
-                makedirs(tempdir)
-                wheretostart = None
-                logger.info('Removing progress file and will start from beginning!')
-            else:
-                logger.info('Environment variable WHERETOSTART = %s' % wheretostart)
+    batch_file = join(folder, 'batch.txt')
+    if exists(batch_file):
+        batch = load_file_to_str(batch_file, strip=True)
+        logger.info('File BATCH = %s' % batch)
+    else:
+        if not batch:
+            batch = get_uuid()
+            logger.info('Generated BATCH = %s' % batch)
+        save_str_to_file(batch, batch_file)
+    return batch
+
+
+@contextlib.contextmanager
+def temp_dir_batch(folder=None, delete_if_exists=False, delete_on_success=True, delete_on_failure=True,
+                   batch=None, tempdir=None):
+    # type: (Optional[str], bool, bool, bool, Optional[str], Optional[str]) -> str
+    """Get a temporary directory optionally with folder appended (and created if it doesn't exist)
+
+    Args:
+        folder (Optional[str]): Folder to create in temporary folder. Defaults to None.
+        delete_if_exists (bool): Whether to delete the folder if it exists. Defaults to False.
+        delete_on_success (bool): Whether to delete folder (if folder supplied) on exiting with statement successfully. Defaults to True.
+        delete_on_failure (bool): Whether to delete folder (if folder supplied) on exiting with statement unsuccessfully. Defaults to True.
+        batch (Optional[str]): Batch to use if there isn't one in a file already.
+        tempdir (Optional[str]): Folder to use as temporary directory. Defaults to None (TEMP_DIR or os.gettempdir).
+
+    Returns:
+        str: A temporary directory
+    """
+    with temp_dir(folder, delete_if_exists, delete_on_success, delete_on_failure, tempdir=tempdir) as tempdir:
+        yield {'folder': tempdir, 'batch': read_or_create_batch(tempdir, batch)}
+
+
+def get_wheretostart(text, message, key):
+    # type: (str, str, str) -> Optional[str]
+    """Evaluate WHERETOSTART.
+
+    Args:
+        text (str): String to process
+        message (str): Text for logging
+        key (str): Key to comapre with
+
+    Returns:
+        Optional[str]: A string or None
+    """
+    upper_text = text.upper()
+    if upper_text == 'RESET':
+        return None
+    if upper_text == 'IGNORE':
+        return 'IGNORE'
+    w_key, wheretostart = text.split('=')
+    if w_key == key:
+        logger.info('%s WHERETOSTART = %s' % (message, wheretostart))
+        return wheretostart
+    else:
+        return 'IGNORE'
+
+
+def progress_storing_folder(info, iterator, key, wheretostart=None):
+    # type: (Dict, Iterable[Dict], str, Optional[str]) -> Tuple[Dict,Dict]
+    """Store progress in folder in key folder of info dictionary parameter. Yields 2 dictionaries. The first is the
+    info dictionary. It contains in key folder the folder being used to store progress and in key progress the current
+    position in the iterator. If store_batch is True, that dictionary will also contain the key batch containing a batch
+    code to be passed as the batch parameter in create_in_hdx or update_in_hdx calls. The second dictionary is the next
+    dictionary in the iterator.
+
+    Args:
+        info (Dict): Dictionary containing folder and anything else to be yielded
+        iterator (Iterable[Dict]): Iterate over this object persisting progress
+        key (str): Key to examine from dictionary from iterator
+        wheretostart (Optional[str]): Where in iterator to start
+
+    Returns:
+        Tuple[Dict,Dict]: A tuple of the form (info dictionary, next object in iterator)
+    """
+    folder = info['folder']
+    progress_file = join(folder, 'progress.txt')
+
+    if not wheretostart:
+        contents = getenv('WHERETOSTART')
+        if contents:
+            wheretostart = get_wheretostart(contents, 'Environment variable', key)
         else:
             if exists(progress_file):
-                wheretostart = load_file_to_str(progress_file, strip=True)
-                logger.info('File WHERETOSTART = %s' % wheretostart)
-        found = False
-        for nextdict in iterator:
-            currentlocation = nextdict[key]
-            if wheretostart and not found:
-                if currentlocation == wheretostart:
+                contents = load_file_to_str(progress_file, strip=True)
+                wheretostart = get_wheretostart(contents, 'File', key)
+            else:
+                wheretostart = None
+    found = False
+    for nextdict in iterator:
+        current = nextdict[key]
+        if wheretostart:
+            if wheretostart == 'IGNORE':
+                continue
+            if not found:
+                if current == wheretostart:
                     found = True
                     logger.info('Starting run from WHERETOSTART %s' % wheretostart)
                 else:
-                    logger.info('Run not started. Ignoring %s. WHERETOSTART (%s) not matched.' % (currentlocation,
+                    logger.info('Run not started. Ignoring %s. WHERETOSTART (%s) not matched.' % (current,
                                                                                                   wheretostart))
                     continue
-            save_str_to_file(currentlocation, progress_file)
-            yield info, nextdict
-        if wheretostart and not found:
-            raise NotFoundError('WHERETOSTART (%s) not matched in iterator and no run started!' % wheretostart)
+        output = '%s=%s' % (key, current)
+        info['progress'] = output
+        save_str_to_file(output, progress_file)
+        yield info, nextdict
+    if wheretostart and not found:
+        raise NotFoundError('WHERETOSTART (%s) not matched in iterator with key %s and no run started!' % (wheretostart, key))
+
+
+def progress_storing_tempdir(folder, iterator, key, batch=None, tempdir=None):
+    # type: (str, Iterable[Dict], str, Optional[str], Optional[str]) -> Tuple[Dict,Dict]
+    """Store progress in temporary directory. The folder persists until the final iteration allowing which iteration to
+    start at and the batch code to be persisted between runs. Yields 2 dictionaries. The first contains key folder which
+    is the temporary directory optionally with folder appended (and created if it doesn't exist). In key progress is
+    held the current position in the iterator. It also contains the key batch containing a batch code to be passed as
+    the batch parameter in create_in_hdx or update_in_hdx calls. The second dictionary is the next dictionary in the
+    iterator.
+
+    Args:
+        folder (str): Folder to create in temporary folder
+        iterator (Iterable[Dict]): Iterate over the iterator persisting progress
+        key (str): Key to examine from dictionary from iterator
+        batch (Optional[str]): Batch to use if there isn't one in a file already.
+        tempdir (Optional[str]): Folder to use as temporary directory. Defaults to None (TEMP_DIR or os.gettempdir).
+
+    Returns:
+        Tuple[Dict,Dict]: A tuple of the form (info dictionary, next object in iterator)
+    """
+    delete_if_exists = False
+    wheretostart = getenv('WHERETOSTART')
+    if wheretostart:
+        if wheretostart.upper() == 'RESET':
+            delete_if_exists = True
+            logger.info('Removing progress file and will start from beginning!')
+    with temp_dir_batch(folder, delete_if_exists, delete_on_success=True, delete_on_failure=False,
+                        batch=batch, tempdir=tempdir) as info:
+        for result in progress_storing_folder(info, iterator, key):
+            yield result
+
+
+def multiple_progress_storing_tempdir(folder, iterators, keys, batch=None):
+    # type: (str, List[Iterable[Dict]], List[str], Optional[str]) -> Tuple[Dict,Dict]
+    """Store progress in temporary directory. The folder persists until the final iteration of the last iterator
+    allowing which iteration to start at and the batch code to be persisted between runs. Yields 2 dictionaries. The
+    first contains key folder which is the temporary directory optionally with folder appended (and created if it
+    doesn't exist). In key progress is held the current position in the iterator. It also contains the key batch
+    containing a batch code to be passed as the batch parameter in create_in_hdx or update_in_hdx calls. The second
+    dictionary is the next dictionary in the iterator.
+
+    Args:
+        folder (str): Folder to create in temporary folder
+        iterators (List[Iterable[Dict]): Iterate over each iterator in the list consecutively persisting progress
+        keys (List[str]): Key to examine from dictionary from each iterator in the above list
+        batch (Optional[str]): Batch to use if there isn't one in a file already.
+
+    Returns:
+        Tuple[int, Dict,Dict]: A tuple of the form (iterator index, info dictionary, next object in iterator)
+    """
+    delete_if_exists = False
+    wheretostart = getenv('WHERETOSTART')
+    if wheretostart:
+        if wheretostart.upper() == 'RESET':
+            delete_if_exists = True
+            logger.info('Removing progress file and will start from beginning!')
+    with temp_dir_batch(folder, delete_if_exists, delete_on_success=True, delete_on_failure=False, batch=batch) as info:
+        tempdir = info['folder']
+        batch = info['batch']
+        for i, key in enumerate(keys):
+            if wheretostart:
+                wheretostart = get_wheretostart(contents, 'Environment variable', key)
+            else:
+                progress_file = join(tempdir, 'progress.txt')
+                if exists(progress_file):
+                    contents = load_file_to_str(progress_file, strip=True)
+                    wheretostart = get_wheretostart(contents, 'File', key)
+                else:
+                    wheretostart = None
+            with temp_dir_batch(str(i), False, delete_on_success=True, delete_on_failure=False,
+                                batch=batch, tempdir=tempdir) as info:
+                for info, nextdict in progress_storing_folder(info, iterators[i], key, wheretostart):
+                    save_str_to_file(info['progress'], progress_file)
+                    yield i, info, nextdict
+                if exists(progress_file):
+                    remove(progress_file)
--- tests/hdx/utilities/test_path.py
+++ tests/hdx/utilities/test_path.py
@@ -1,5 +1,6 @@
 # -*- coding: UTF-8 -*-
 """Path Utility Tests"""
+import copy
 from os.path import join, exists
 from shutil import rmtree
 from tempfile import gettempdir
@@ -7,7 +8,7 @@ from tempfile import gettempdir
 import pytest
 
 from hdx.utilities.loader import load_file_to_str
-from hdx.utilities.path import get_temp_dir, temp_dir, progress_storing_tempdir
+from hdx.utilities.path import get_temp_dir, temp_dir, progress_storing_tempdir, multiple_progress_storing_tempdir
 
 
 class TestPath:
@@ -95,36 +96,30 @@ class TestPath:
         rmtree(expected_dir, ignore_errors=True)
         iterator = [{'iso3': 'AFG', 'name': 'Afghanistan'}, {'iso3': 'SDN', 'name': 'Sudan'},
                     {'iso3': 'YEM', 'name': 'Yemen'}, {'iso3': 'ZAM', 'name': 'Zambia'}]
-        result = list()
-        for info, nextdict in progress_storing_tempdir(tempfolder, iterator, 'iso3'):
-            assert exists(info['tempdir']) is True
-            assert info['tempdir'] == expected_dir
-            result.append(nextdict)
-        assert result == iterator
-        assert exists(expected_dir) is False
-
         expected_batch_file = join(expected_dir, 'batch.txt')
         result = list()
-        for info, nextdict in progress_storing_tempdir(tempfolder, iterator, 'iso3', store_batch=True):
-            assert info['tempdir'] == expected_dir
+        for info, nextdict in progress_storing_tempdir(tempfolder, iterator, 'iso3'):
+            assert info['folder'] == expected_dir
             expected_batch = load_file_to_str(expected_batch_file, strip=True)
             result.append(nextdict)
         assert result == iterator
         assert expected_batch == info['batch']
         assert exists(expected_dir) is False
 
-        monkeypatch.setenv('WHERETOSTART', 'SDN')
+        monkeypatch.setenv('WHERETOSTART', 'iso3=SDN')
         result = list()
         for info, nextdict in progress_storing_tempdir(tempfolder, iterator, 'iso3'):
-            assert exists(info['tempdir']) is True
-            assert info['tempdir'] == expected_dir
+            assert exists(info['folder']) is True
+            assert info['folder'] == expected_dir
+            expected_batch = load_file_to_str(expected_batch_file, strip=True)
             result.append(nextdict)
         assert result == iterator[1:]
+        assert expected_batch == info['batch']
         assert exists(expected_dir) is False
         monkeypatch.delenv('WHERETOSTART')
 
         try:
-            for info, nextdict in progress_storing_tempdir(tempfolder, iterator, 'iso3', store_batch=True):
+            for info, nextdict in progress_storing_tempdir(tempfolder, iterator, 'iso3'):
                 if nextdict['iso3'] == 'YEM':
                     start_batch = info['batch']
                     raise ValueError('Problem!')
@@ -132,9 +127,9 @@ class TestPath:
             pass
         assert exists(expected_dir) is True
         result = list()
-        for info, nextdict in progress_storing_tempdir(tempfolder, iterator, 'iso3', store_batch=True):
-            assert exists(info['tempdir']) is True
-            assert info['tempdir'] == expected_dir
+        for info, nextdict in progress_storing_tempdir(tempfolder, iterator, 'iso3'):
+            assert exists(info['folder']) is True
+            assert info['folder'] == expected_dir
             assert info['batch'] == start_batch
             result.append(nextdict)
         assert result == iterator[2:]
@@ -143,6 +138,7 @@ class TestPath:
         try:
             for info, nextdict in progress_storing_tempdir(tempfolder, iterator, 'iso3'):
                 if nextdict['iso3'] == 'YEM':
+                    start_batch = info['batch']
                     raise ValueError('Problem!')
         except:
             pass
@@ -150,8 +146,9 @@ class TestPath:
         monkeypatch.setenv('WHERETOSTART', 'RESET')
         result = list()
         for info, nextdict in progress_storing_tempdir(tempfolder, iterator, 'iso3'):
-            assert exists(info['tempdir']) is True
-            assert info['tempdir'] == expected_dir
+            assert exists(info['folder']) is True
+            assert info['folder'] == expected_dir
+            assert info['batch'] != start_batch
             result.append(nextdict)
         assert result == iterator
         assert exists(expected_dir) is False
@@ -160,30 +157,42 @@ class TestPath:
         try:
             for info, nextdict in progress_storing_tempdir(tempfolder, iterator, 'iso3'):
                 if nextdict['iso3'] == 'YEM':
+                    start_batch = info['batch']
                     raise ValueError('Problem!')
         except:
             pass
         assert exists(expected_dir) is True
-        monkeypatch.setenv('WHERETOSTART', 'SDN')
+        monkeypatch.setenv('WHERETOSTART', 'iso3=SDN')
         result = list()
         for info, nextdict in progress_storing_tempdir(tempfolder, iterator, 'iso3'):
-            assert exists(info['tempdir']) is True
-            assert info['tempdir'] == expected_dir
+            assert exists(info['folder']) is True
+            assert info['folder'] == expected_dir
+            assert info['batch'] == start_batch
             result.append(nextdict)
         assert result == iterator[1:]
         assert exists(expected_dir) is False
         monkeypatch.delenv('WHERETOSTART')
 
         try:
-            for info, nextdict in progress_storing_tempdir(tempfolder, iterator, 'iso3', store_batch=True):
+            for info, nextdict in progress_storing_tempdir(tempfolder, iterator, 'iso3'):
                 if nextdict['iso3'] == 'YEM':
                     start_batch = info['batch']
                     raise ValueError('Problem!')
         except:
             pass
-        monkeypatch.setenv('WHERETOSTART', 'NOTFOUND')
+        monkeypatch.setenv('WHERETOSTART', 'iso3=NOTFOUND')
         found = False
-        for _ in progress_storing_tempdir(tempfolder, iterator, 'iso3', store_batch=True):
+        for _ in progress_storing_tempdir(tempfolder, iterator, 'iso3'):
+            found = True
+        assert found is False
+        assert exists(expected_dir) is True
+        batch = load_file_to_str(expected_batch_file, strip=True)
+        assert batch == start_batch
+        monkeypatch.delenv('WHERETOSTART')
+
+        monkeypatch.setenv('WHERETOSTART', 'NOTFOUND=SDN')
+        found = False
+        for _ in progress_storing_tempdir(tempfolder, iterator, 'iso3'):
             found = True
         assert found is False
         assert exists(expected_dir) is True
@@ -193,3 +202,46 @@ class TestPath:
 
         rmtree(expected_dir, ignore_errors=True)
 
+    def test_multiple_progress_storing_tempdir(self, monkeypatch):
+        tempfolder = 'gaga'
+        expected_dir = join(gettempdir(), tempfolder)
+        rmtree(expected_dir, ignore_errors=True)
+        iterator1 = [{'emergency_id': '911'}]
+        iterator2 = [{'iso3': 'AFG', 'name': 'Afghanistan'}, {'iso3': 'SDN', 'name': 'Sudan'},
+                     {'iso3': 'YEM', 'name': 'Yemen'}, {'iso3': 'ZAM', 'name': 'Zambia'}]
+        iterators = [iterator1, iterator2]
+        keys = ['emergency_id', 'iso3']
+        results = list()
+        for result in multiple_progress_storing_tempdir(tempfolder, iterators, keys, '1234'):
+            results.append(copy.deepcopy(result))
+        expected_results = [(0, {'folder': '/tmp/gaga/0', 'batch': '1234', 'progress': 'emergency_id=911'}, {'emergency_id': '911'}),
+                            (1, {'folder': '/tmp/gaga/1', 'batch': '1234', 'progress': 'iso3=AFG'}, {'iso3': 'AFG', 'name': 'Afghanistan'}),
+                            (1, {'folder': '/tmp/gaga/1', 'batch': '1234', 'progress': 'iso3=SDN'}, {'iso3': 'SDN', 'name': 'Sudan'}),
+                            (1, {'folder': '/tmp/gaga/1', 'batch': '1234', 'progress': 'iso3=YEM'}, {'iso3': 'YEM', 'name': 'Yemen'}),
+                            (1, {'folder': '/tmp/gaga/1', 'batch': '1234', 'progress': 'iso3=ZAM'}, {'iso3': 'ZAM', 'name': 'Zambia'})]
+        assert results == expected_results
+        assert exists(expected_dir) is False
+        results = list()
+        try:
+            for result in multiple_progress_storing_tempdir(tempfolder, iterators, keys):
+                results.append(copy.deepcopy(result))
+                i, info, nextdict = result
+                if 'iso3' in nextdict and nextdict['iso3'] == 'YEM':
+                    start_batch = info['batch']
+                    raise ValueError('Problem!')
+        except:
+            pass
+        for result in expected_results:
+            result[1]['batch'] = start_batch
+        assert results == expected_results[:4]
+        assert exists(expected_dir) is True
+        result = list()
+        for i, info, nextdict in multiple_progress_storing_tempdir(tempfolder, iterators, keys):
+            assert exists(info['folder']) is True
+            assert info['folder'] == join(expected_dir, '1')
+            assert info['batch'] == start_batch
+            result.append(nextdict)
+        assert result == iterator2[2:]
+        assert exists(expected_dir) is False
+
+        rmtree(expected_dir, ignore_errors=True)
