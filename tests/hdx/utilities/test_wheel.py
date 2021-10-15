b42dbc58381f3822ad59d1081e6c16d3ae0acb9a
Mike <rans@un.org>
2021-10-15 14:58:46 +1300

    Fix test

--- tests/hdx/utilities/test_wheel.py
+++ tests/hdx/utilities/test_wheel.py
@@ -9,7 +9,7 @@ from hdx.utilities.wheel import get_version_from_whl
 class TestWheel:
     @pytest.fixture(scope="class")
     def version_folder(self, fixturesfolder):
-        return join(fixturesfolder, "version")
+        return join(fixturesfolder, "wheel")
 
     def test_get_version_from_whl(self, version_folder):
         assert get_version_from_whl(version_folder) == "3.0.2"
