6c42e9be4989c14a906d9f3b85332f70ca2ed12f
Mike <rans@un.org>
2023-01-18 08:32:59 +1300

    Update to Frictionless 5

--- src/hdx/utilities/frictionless_wrapper.py
+++ src/hdx/utilities/frictionless_wrapper.py
@@ -1,11 +1,109 @@
-from typing import Any, Optional
+"""Frictionless wrapper"""
+from typing import Any, Optional, Tuple
 
-import frictionless
 import requests
-from frictionless import FrictionlessException
+from frictionless import (
+    Control,
+    Detector,
+    Dialect,
+    FrictionlessException,
+    Resource,
+    system,
+)
 from frictionless.errors import ResourceError
-from frictionless.plugins.csv import CsvDialect
-from frictionless.plugins.excel import ExcelDialect
+from frictionless.formats import CsvControl, ExcelControl
+
+
+def get_frictionless_control(**kwargs: Any) -> Tuple[Control, Any]:
+    """Get Frictionless Control.
+
+    Args:
+        **kwargs:
+        file_type (Optional[str]): Type of file. Defaults to inferring.
+        format (Optional[str]): Type of file. Defaults to inferring.
+        delimiter (Optional[str]): Delimiter for values in csv rows. Defaults to inferring.
+        skip_initial_space (bool): Ignore whitespace straight after delimiter. Defaults to False.
+        sheet (Optional[Union[int, str]): Sheet in Excel. Defaults to inferring.
+        fill_merged_cells (bool): Whether to fill merged cells. Defaults to True.
+        control (Control): This can be set to override the above. See Frictionless docs.
+
+    Returns:
+        Tuple[Control, Any]: (frictionless Control object, kwargs)
+    """
+    control = kwargs.get("control")
+    file_type = kwargs.pop("file_type", None)
+    file_format = kwargs.get("format", file_type)
+    if file_format is not None:
+        kwargs["format"] = file_format
+        if control is None:
+            if file_format == "csv":
+                control = CsvControl()
+                delimiter = kwargs.pop("delimiter", None)
+                if delimiter is not None:
+                    control.delimiter = delimiter
+                skip_initial_space = kwargs.pop("skip_initial_space", None)
+                if skip_initial_space is not None:
+                    control.skip_initial_space = skip_initial_space
+            elif file_format in ("xls", "xlsx"):
+                control = ExcelControl()
+                sheet = kwargs.pop("sheet", None)
+                if sheet is not None:
+                    control.sheet = sheet
+                fill_merged_cells = kwargs.pop("fill_merged_cells", True)
+                control.fill_merged_cells = fill_merged_cells
+    return control, kwargs
+
+
+def get_frictionless_detector(
+    infer_types: bool, **kwargs: Any
+) -> Tuple[Detector, Any]:
+    """Get Frictionless Detector.
+
+    Args:
+        infer_types (bool): Whether to infer types. Defaults to False (strings).
+        **kwargs:
+        default_type (Optional[str]): Default field type if infer_types False. Defaults to any.
+        float_numbers (bool): Use float not Decimal if infer_types True. Defaults to True.
+        null_values (List[Any]): Values that will return None. Defaults to [""].
+        detector (Detector): This can be set to override the above. See Frictionless docs.
+
+    Returns:
+        Tuple[Detector, Any]: (frictionless Detector object, kwargs)
+    """
+    detector = kwargs.get("detector", Detector())
+    if infer_types:
+        default = None
+    else:
+        default = "any"
+    default_type = kwargs.pop("default_type", default)
+    detector.field_type = default_type
+    float_numbers = kwargs.pop("float_numbers", True)
+    detector.field_float_numbers = float_numbers
+    null_values = kwargs.pop("null_values", [""])
+    detector.field_missing_values = null_values
+    return detector, kwargs
+
+
+def get_frictionless_dialect(
+    ignore_blank_rows: bool, **kwargs: Any
+) -> Tuple[Dialect, Any]:
+    """Get Frictionless Dialect.
+
+    Args:
+        ignore_blank_rows (bool): Whether to ignore blank rows. Defaults to True.
+        **kwargs:
+        columns (Union[ListTuple[int], ListTuple[str], None]): Columns to pick. Defaults to all.
+        dialect (Dialect): This can be set to override the above. See Frictionless docs.
+
+    Returns:
+        Tuple[Dialect, Any]: (frictionless Dialect object, Any)
+    """
+    dialect = kwargs.get("dialect", Dialect())
+    columns = kwargs.pop("columns", None)
+    if columns:
+        dialect.pick_fields = columns
+    dialect.skip_blank_rows = ignore_blank_rows
+    return dialect, kwargs
 
 
 def get_frictionless_resource(
@@ -15,7 +113,7 @@ def get_frictionless_resource(
     session: Optional[requests.Session] = None,
     data: Optional[Any] = None,
     **kwargs: Any,
-) -> frictionless.Resource:
+) -> Resource:
     """Get Frictionless Resource. Either url or data must be supplied.
 
     Args:
@@ -26,7 +124,7 @@ def get_frictionless_resource(
         data (Optional[Any]): Data to parse. Defaults to None.
         **kwargs:
         has_header (bool): Whether data has a header. Defaults to True.
-        headers (Union[int, ListTuple[int], ListTuple[str]]): Number of row(s) containing headers or list of headers
+        headers (Union[int, ListTuple[int], ListTuple[str]]): Number of row(s) containing headers or list of headers.  # pylint: disable=line-too-long
         columns (Union[ListTuple[int], ListTuple[str], None]): Columns to pick. Defaults to all.
         file_type (Optional[str]): Type of file. Defaults to inferring.
         format (Optional[str]): Type of file. Defaults to inferring.
@@ -40,81 +138,42 @@ def get_frictionless_resource(
         default_type (Optional[str]): Default field type if infer_types False. Defaults to any.
         float_numbers (bool): Use float not Decimal if infer_types True. Defaults to True.
         null_values (List[Any]): Values that will return None. Defaults to [""].
-        dialect (Dialect): This can be set to override the above. See Frictionless docs.
+        control (Control): This can be set to override the above. See Frictionless docs.
         detector (Detector): This can be set to override the above. See Frictionless docs.
-        layout (Layout): This can be set to override the above. See Frictionless docs.
+        dialect (Dialect): This can be set to override the above. See Frictionless docs.
         schema (Schema): This can be set to override the above. See Frictionless docs.
 
     Returns:
-        frictionless.Resource: frictionless Resource object
+        Resource: frictionless Resource object
     """
     if not url and not data:
         error = ResourceError(note="Neither url or data supplied!")
         raise FrictionlessException(error=error)
-    file_type = kwargs.pop("file_type", None)
-    format = kwargs.get("format", file_type)
-    dialect = kwargs.get("dialect")
-    if format is not None:
-        kwargs["format"] = format
-        if dialect is None:
-            if format == "csv":
-                dialect = CsvDialect()
-                delimiter = kwargs.pop("delimiter", None)
-                if delimiter is not None:
-                    dialect.delimiter = delimiter
-                skip_initial_space = kwargs.pop("skip_initial_space", None)
-                if skip_initial_space is not None:
-                    dialect.skip_initial_space = skip_initial_space
-            elif format in ("xls", "xlsx"):
-                dialect = ExcelDialect()
-                sheet = kwargs.pop("sheet", None)
-                if sheet is not None:
-                    dialect.sheet = sheet
-                fill_merged_cells = kwargs.pop("fill_merged_cells", True)
-                dialect.fill_merged_cells = fill_merged_cells
-    detector = kwargs.get("detector", frictionless.Detector())
-    if infer_types:
-        default = None
-    else:
-        default = "any"
-    default_type = kwargs.pop("default_type", default)
-    detector.field_type = default_type
-    float_numbers = kwargs.pop("float_numbers", True)
-    detector.field_float_numbers = float_numbers
-    null_values = kwargs.pop("null_values", [""])
-    detector.field_missing_values = null_values
-    layout = kwargs.get("layout", frictionless.Layout())
+    control, kwargs = get_frictionless_control(**kwargs)
+    detector, kwargs = get_frictionless_detector(infer_types, **kwargs)
+    dialect, kwargs = get_frictionless_dialect(ignore_blank_rows, **kwargs)
     has_header = kwargs.pop("has_header", None)
     headers = kwargs.pop("headers", None)
     if headers is not None:
         if isinstance(headers, int):
             headers = [headers]
         if isinstance(headers[0], int):
-            layout.header_rows = headers
+            dialect.header_rows = headers
         else:
-            detector._Detector__field_names = headers
+            detector.field_names = headers
             if has_header is None:
                 has_header = False
     if has_header is None:
         has_header = True
-    layout.header = has_header
-    columns = kwargs.pop("columns", None)
-    if columns:
-        layout.pick_fields = columns
-    if ignore_blank_rows:
-        if layout.skip_rows is None:
-            layout.skip_rows = ["<blank>"]
-        elif "<blank>" not in layout.skip_rows:
-            layout.skip_rows.append("<blank>")
-    kwargs["dialect"] = dialect
+    dialect.header = has_header
+    kwargs["control"] = control
     kwargs["detector"] = detector
-    kwargs["layout"] = layout
+    kwargs["dialect"] = dialect
     http_session = kwargs.pop("http_session", session)
-    if http_session is not None:
-        frictionless.system.use_http_session(http_session)
-    if url:
-        resource = frictionless.Resource(path=url, **kwargs)
-    else:
-        resource = frictionless.Resource(data=data, **kwargs)
-    resource.open()
-    return resource
+    with system.use_context(http_session=http_session):
+        if url:
+            resource = Resource(path=url, **kwargs)
+        else:
+            resource = Resource(data=data, **kwargs)
+        resource.open()
+        return resource
