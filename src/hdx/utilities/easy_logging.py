fd0129ffed040d9ba40d9917ac8f2d5c36654eb3
Mike <rans@un.org>
2023-03-28 09:14:59 +1300

    Autoescape <locals> to allow standard logger to be used by tenacity through loguru Python 3.11 support for loguru

--- src/hdx/utilities/easy_logging.py
+++ src/hdx/utilities/easy_logging.py
@@ -1,6 +1,7 @@
 """Configuration of logging."""
 import logging
 import logging.config
+import sys
 from sys import stderr
 from typing import Optional
 
@@ -42,27 +43,25 @@ def setup_logging(
 
     class InterceptHandler(logging.Handler):
         def emit(self, record):
-            # Get corresponding Loguru level if it exists
+            # Get corresponding Loguru level if it exists.
             try:
                 level = logger.level(record.levelname).name
             except ValueError:
                 level = record.levelno
 
-            # Find caller from where originated the logged message
-            frame, depth = logging.currentframe(), 2
-            while frame.f_code.co_filename == logging.__file__:
+            # Find caller from where originated the logged message.
+            frame, depth = sys._getframe(6), 6
+            while frame and frame.f_code.co_filename == logging.__file__:
                 frame = frame.f_back
                 depth += 1
 
             msg = record.getMessage().replace("<locals>", "\\<locals>")
             logger.opt(
-                colors=True,
-                depth=depth,
-                exception=record.exc_info,
+                colors=True, depth=depth, exception=record.exc_info
             ).log(level, msg)
 
     root_logger = logging.getLogger()
     while root_logger.hasHandlers():
         root_logger.removeHandler(root_logger.handlers[0])
     root_logger.setLevel(logging.NOTSET)
-    logging.basicConfig(handlers=[InterceptHandler()], level=logging.NOTSET)
+    logging.basicConfig(handlers=[InterceptHandler()], level=0, force=True)
