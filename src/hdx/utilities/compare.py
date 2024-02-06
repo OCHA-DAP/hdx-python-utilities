0d4680140292d2cd07f4c510a3c2e4fd5416fcd0
Ian Hopkinson <ianhopkinson@googlemail.com>
2024-02-06 06:55:18 +0000

    Add encoding to compare_files functions

--- src/hdx/utilities/compare.py
+++ src/hdx/utilities/compare.py
@@ -4,7 +4,7 @@ from os import linesep
 from typing import List
 
 
-def compare_files(path1: str, path2: str) -> List[str]:
+def compare_files(path1: str, path2: str, encoding: str = "utf-8") -> List[str]:
     """Returns the delta between two files using -, ?, + format excluding lines
     that are the same.
 
@@ -16,12 +16,13 @@ def compare_files(path1: str, path2: str) -> List[str]:
         List[str]: Delta between the two files
     """
     diff = difflib.ndiff(
-        open(path1).read().splitlines(), open(path2).read().splitlines()
+        open(path1, encoding=encoding).read().splitlines(),
+        open(path2, encoding=encoding).read().splitlines(),
     )
     return [x for x in diff if x[0] in ["-", "+", "?"]]
 
 
-def assert_files_same(path1: str, path2: str) -> None:
+def assert_files_same(path1: str, path2: str, encoding: str = "utf-8") -> None:
     """Asserts that two files are the same and returns delta using.
 
     -, ?, + format if not
@@ -33,5 +34,5 @@ def assert_files_same(path1: str, path2: str) -> None:
     Returns:
         None
     """
-    difflines = compare_files(path1, path2)
+    difflines = compare_files(path1, path2, encoding)
     assert len(difflines) == 0, linesep.join([linesep] + difflines)
